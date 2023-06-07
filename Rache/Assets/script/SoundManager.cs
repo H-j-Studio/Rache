92e0d80ce3da651c4e8668e3b6eed3b307b35e2e
NANONANDFLASH <68502584+NANONANDFLASH@users.noreply.github.com>
2023-06-07 23:40:11 +0900

    ui ux

--- Rache/Assets/script/SoundManager.cs
+++ Rache/Assets/script/SoundManager.cs
@@ -6,18 +6,6 @@ using UnityEngine.UI;
 public class SoundManager : MonoBehaviour
 {
     public AudioSource musicsourse;
-    public Button Quit_bt;
-    public GameObject panel;
-
-    void Start()
-    {
-        Quit_bt.onClick.AddListener(DownPanel);
-    }
-
-    private void DownPanel()
-    {
-        panel.SetActive(false);
-    }
 
     public void SetMusicVolume(float volume)
     {
